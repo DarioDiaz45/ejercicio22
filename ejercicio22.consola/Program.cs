747a82e024e589c3f68502c562d0033c071c6d6e
DarioDiaz45 <nicolas2004diaz@gmail.com>
2023-07-10 21:44:36 -0300

    Cambios

--- ejercicio22.consola/Program.cs
+++ ejercicio22.consola/Program.cs
@@ -4,7 +4,10 @@ Console.Write("ingrese la temperatura");
 var temperatura=double.Parse(Console.ReadLine());
 Console.Write("ingrese la escala de la temperatura (c)elcius o (f)ahrenheit");
 var escala=Console.ReadLine();
-if (escala.ToUpper() == "c") ;
+if (escala.ToUpper() == "c")
+{
+
+}
     var fahrenheit = ConvertirCelciusFareheit(temperatura);
 
 Console.WriteLine($"{temperatura}grados f equivalen a {celcius}grados";
