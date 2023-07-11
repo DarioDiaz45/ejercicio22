977b765379d08b6c43c9e78a3c49306c0d0cf015
cristian <cristian@DESKTOP-GOUDOTN>
2023-07-10 21:21:23 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio22.consola/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("conversion de temperaturas");
+Console.Write("ingrese la temperatura");
+var temperatura=double.Parse(Console.ReadLine());
+Console.Write("ingrese la escala de la temperatura (c)elcius o (f)ahrenheit");
+var escala=Console.ReadLine();
+if (escala.ToUpper() == "c") ;
+    var fahrenheit = ConvertirCelciusFareheit(temperatura);
+
+Console.WriteLine($"{temperatura}grados f equivalen a {celcius}grados";
+
+
+
+
